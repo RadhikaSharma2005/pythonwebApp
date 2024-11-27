8cc8500b581803aaefe1b643441412bdeafc1dcb
RadhikaSharma2005 <113759147+RadhikaSharma2005@users.noreply.github.com>
2024-11-28 00:21:34 +0530

    Update f1.py

--- f1.py
+++ f1.py
@@ -4,7 +4,7 @@ from sklearn.metrics import accuracy_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.model_selection import train_test_split
 
-df = pd.read_csv(r"./diabetes.csv")
+df = pd.read_csv(r"./diabetes_prediction_dataset.csv")
 
 # Updated Header with Modern Styling
 st.markdown(
